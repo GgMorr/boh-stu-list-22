cf2b4b2db5a4442af93f57ed4cf9833ace9b66ae
Batya <Gigi.ucla21@gmail.com>
2022-07-07 16:07:17 -0700

    first commit

--- Desktop/BOH_STU_LIST_22/backend/index.js
+++ Desktop/BOH_STU_LIST_22/backend/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyparser from 'body-parser';
 
 
 const app = express();
 const PORT = 3000;
+
+//mongo connetion
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/bohDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//bodyparser setup
+app.use(bodyparser.urlencoded({ extended: true}));
+app.use(bodyparser.json());
 
 app.get('/', (req, res) => 
     res.send(`The BOH application is running on port ${PORT}`)
